e2a44d8137553f9db18cf10d93107748f78638a1
LOTZ <william.lotz64@gmail.com>
2026-01-05 16:22:55 +0100

    classe sucre et oxy

--- target/classes/com/example/Oxymetre.java
+++ target/classes/com/example/Oxymetre.java
@@ -1,3 +1,17 @@
 public class Oxymetre {
     
+    private double saturationOxygene;
+
+    public Oxymetre( double saturationOxygene) {
+       
+        this.saturationOxygene = saturationOxygene;
+    }
+
+    public double getSaturationOxygene() {
+        return saturationOxygene;
+    }
+
+    public void setSaturationOxygene(double saturationOxygene) {
+        this.saturationOxygene = saturationOxygene;
+    }
 }
